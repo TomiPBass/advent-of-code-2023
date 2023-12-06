4d2f282187ca2dd49f1dc2fb1a610f070c6a077d
TomiP <pizik3@seznam.cz>
2023-12-07 00:10:04 +0100

    day 5 done

--- day-5/1.ts
+++ day-5/1.ts
@@ -1,4 +1,38 @@
-import { input, testingInput } from './input';
+import { input } from './input';
+
+export const testingInput = `seeds: 79 80 81 82 83 84 85 86 87 88 89 90 91 92 55 56 57 58 59 60 61 62 63 64 65 66 67 
+
+seed-to-soil map:
+50 98 2
+52 50 48
+
+soil-to-fertilizer map:
+0 15 37
+37 52 2
+39 0 15
+
+fertilizer-to-water map:
+49 53 8
+0 11 42
+42 0 7
+57 7 4
+
+water-to-light map:
+88 18 7
+18 25 70
+
+light-to-temperature map:
+45 77 23
+81 45 19
+68 64 13
+
+temperature-to-humidity map:
+0 69 1
+1 0 69
+
+humidity-to-location map:
+60 56 37
+56 93 4`;
 
 const start = new Date().getTime();
 const mapNames = [
@@ -11,7 +45,7 @@ const mapNames = [
     'humidity-to-location',
 ] as const;
 
-const lines = input.split('\n');
+const lines = testingInput.split('\n');
 
 type MapName = (typeof mapNames)[number];
 
@@ -76,9 +110,9 @@ const mapAllSets = (maps: number[][]) => {
     return currentSet;
 };
 
-const lastMap = mapAllSets(parsedInput[mapNames[itteration]]);
+export const lastMap = mapAllSets(parsedInput[mapNames[itteration]]);
 const lowestNumber = Math.min(...lastMap);
-console.log('lowest number', lowestNumber);
+// console.log('lowest number', lowestNumber);
 
 const end = new Date().getTime();
-console.log(`Execution time: ${end - start} ms`);
+// console.log(`Execution time: ${end - start} ms`);
---
+++ day-5/2.ts
@@ -0,0 +1,136 @@
+import { input, testingInput } from './input';
+
+const start = new Date().getTime();
+const mapNames = [
+    'seed-to-soil',
+    'soil-to-fertilizer',
+    'fertilizer-to-water',
+    'water-to-light',
+    'light-to-temperature',
+    'temperature-to-humidity',
+    'humidity-to-location',
+] as const;
+
+const lines = input.split('\n');
+
+type MapName = (typeof mapNames)[number];
+
+const findMapLines = (lines: string[], map: MapName) => {
+    return lines.filter((line) => {
+        let [startLineIndex, endLineIndex] = [0, 0];
+        const indexOfProvidedMap = mapNames.indexOf(map);
+        lines.forEach((line, index) => {
+            if (line.includes(mapNames[indexOfProvidedMap]))
+                startLineIndex = index + 1;
+            if (mapNames[indexOfProvidedMap + 1]) {
+                if (line.includes(mapNames[indexOfProvidedMap + 1]))
+                    endLineIndex = index - 2;
+            } else endLineIndex = lines.length - 1;
+        });
+        return (
+            lines.indexOf(line) >= startLineIndex &&
+            lines.indexOf(line) <= endLineIndex
+        );
+    });
+};
+
+const mapEachLine = (mapLine: string[]) => {
+    return mapLine.map((line) => line.split(' ').map(Number));
+};
+
+const originalSeeds = lines[0].split(' ').slice(1).map(Number);
+
+const parseInput = () => {
+    const mapLines = mapNames.map((map) => findMapLines(lines, map));
+    return {
+        originalSeeds,
+        ['seed-to-soil']: mapEachLine(mapLines[0]),
+        ['soil-to-fertilizer']: mapEachLine(mapLines[1]),
+        ['fertilizer-to-water']: mapEachLine(mapLines[2]),
+        ['water-to-light']: mapEachLine(mapLines[3]),
+        ['light-to-temperature']: mapEachLine(mapLines[4]),
+        ['temperature-to-humidity']: mapEachLine(mapLines[5]),
+        ['humidity-to-location']: mapEachLine(mapLines[6]),
+    };
+};
+
+type Set = { seed: number; range: number };
+
+const parsedInput = parseInput();
+const parsedSeeds: Set[] = parsedInput.originalSeeds
+    .map((seedNumber, index) => {
+        const obj = {} as Set;
+        if (index % 2 === 0) {
+            obj.seed = seedNumber;
+            obj.range = parsedInput.originalSeeds[index + 1];
+            return obj;
+        }
+    })
+    .filter((obj) => obj);
+
+let parsedSeedIndex = 0;
+let currentSeedMapping = parsedSeeds[0].seed;
+let seedForMapping = parsedSeeds[0].seed;
+let lowestMappedSeedYet: undefined | number = undefined;
+let seedIteration = 0;
+let mapItteration = 0;
+let setToSetMap: [number, number] = [undefined, undefined];
+
+const mapAllSets = (maps: number[][]) => {
+    if (mapItteration === mapNames.length) return;
+    maps.forEach((map) => {
+        if (seedForMapping >= map[1] && seedForMapping <= map[1] + map[2] - 1) {
+            setToSetMap = [seedForMapping, seedForMapping - map[1] + map[0]];
+        }
+    });
+    if (setToSetMap.includes(undefined)) {
+        setToSetMap = [seedForMapping, seedForMapping];
+    }
+    seedForMapping = setToSetMap[1];
+    mapItteration++;
+    mapAllSets(parsedInput[mapNames[mapItteration]]);
+    if (
+        seedForMapping < lowestMappedSeedYet ||
+        lowestMappedSeedYet === undefined
+    ) {
+        lowestMappedSeedYet = seedForMapping;
+    }
+    if (mapItteration !== 0) {
+        seedIteration++;
+    }
+    mapItteration = 0;
+    setToSetMap = [undefined, undefined];
+    return;
+};
+
+const mappingFunc = () => {
+    mapAllSets(parsedInput[mapNames[mapItteration]]);
+    while (
+        parsedSeeds[parsedSeedIndex].seed + seedIteration <=
+        parsedSeeds[parsedSeedIndex].seed +
+            parsedSeeds[parsedSeedIndex].range -
+            1
+    ) {
+        seedForMapping = currentSeedMapping + seedIteration;
+        mapAllSets(parsedInput[mapNames[mapItteration]]);
+    }
+    parsedSeedIndex++;
+    console.log('parsedSeedIndex', parsedSeedIndex);
+    seedIteration = 0;
+    if (parsedSeedIndex === parsedSeeds.length) return;
+    else {
+        seedForMapping = parsedSeeds[parsedSeedIndex].seed;
+        currentSeedMapping = parsedSeeds[parsedSeedIndex].seed;
+    }
+    mappingFunc();
+};
+
+mappingFunc();
+
+console.log('lowest number', lowestMappedSeedYet);
+
+const end = new Date().getTime();
+console.log(`Execution time: ${end - start} ms`);
+
+// const firstIncorrectResult = `31555917`;
+// const secondCorrectResult = ``;
